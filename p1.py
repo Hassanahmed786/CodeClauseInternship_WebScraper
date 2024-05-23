cd72620b26edb81cfb826c50050e3eb790af1936
SHAIK HASSAN AHMED <146012696+Hassanahmed786@users.noreply.github.com>
2024-05-24 00:28:25 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL of news website
+url = 'https://www.webscraper.io/test-sites/e-commerce/allinone'
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+headlines = soup.find_all('h2', class_='headline')
+
+for headline in headlines:
+    print(headline.text.strip())
+
